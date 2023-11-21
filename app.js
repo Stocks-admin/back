c5d17abe4d307b52585c99e406b2ee92ce640337
Matias Duran <matiduraan@gmail.com>
2023-11-20 23:14:52 -0300

    Change CORS

--- app.js
+++ app.js
@@ -11,7 +11,10 @@ const db = new PrismaClient();
 const app = express();
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "development.d2jiei2auzx96a.amplifyapp.com"
+  );
   res.setHeader(
     "Access-Control-Allow-Methods",
     "GET, POST, PATCH, PUT, DELETE, OPTIONS"
